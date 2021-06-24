ceeac7aa67eb025a66d8351ab2d933b26dc8e7dc
tinyuliu <tinyu1016@gmail.com>
2021-06-24 12:49:39 +0800

    add an error test case

--- test/test.js
+++ test/test.js
@@ -4,5 +4,8 @@ describe('Array', function(){
         it('should return -1 when the value is not present', function(){
             assert.equal(-1, [1, 2, 3].indexOf((0)));
         })
+        it('should return error', function(){
+            assert.equal(3, [1, 2, 3].indexOf((5)));
+        })
     })
 })
